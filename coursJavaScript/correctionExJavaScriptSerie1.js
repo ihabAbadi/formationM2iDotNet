9e2da13f85985c39499753b9984ae1756ce57dfd
ihab@utopios.net <ihab@utopios.net>
2020-06-25 15:36:12 +0200

    correction ex3

--- coursJavaScript/correctionExJavaScriptSerie1.js
+++ coursJavaScript/correctionExJavaScriptSerie1.js
@@ -10,16 +10,27 @@
 //     }
 //     result.innerText = "Le resultat est de "+ somme
 // })
+//Exercice 2
+// const input = document.querySelector('input')
+// const result = document.querySelector("#result")
 
-const input = document.querySelector('input')
-const result = document.querySelector("#result")
+// input.addEventListener('keyup', function(e) {
+//     let age = parseInt(e.target.value)
+//     if(age > 0 && age < 130) {
+//         result.innerHTML = "Age correct"
+//     }
+//     else {
+//         result.innerHTML = "Age incorrect, merci de ressayer"
+//     }
+// })
+
+//Exercice 3
+const input = document.querySelector("input")
+const result = document.querySelector("div")
 
 input.addEventListener('keyup', function(e) {
-    let age = parseInt(e.target.value)
-    if(age > 0 && age < 130) {
-        result.innerHTML = "Age correct"
-    }
-    else {
-        result.innerHTML = "Age incorrect, merci de ressayer"
+    result.innerHTML = "";
+    for(let i=1; i <= parseInt(e.target.value); i++) {
+        result.innerHTML += "<div>Bonjour tout le monde</div>"
     }
 })
