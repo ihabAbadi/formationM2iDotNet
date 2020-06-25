e807610e77a1e7afcbcbdd23a7bea7b41caeb650
ihab@utopios.net <ihab@utopios.net>
2020-06-25 14:43:12 +0200

    correction exercice 2

--- coursJavaScript/correctionExJavaScriptSerie1.js
+++ coursJavaScript/correctionExJavaScriptSerie1.js
@@ -1,11 +1,25 @@
-const allInput = document.querySelectorAll('input')
-const result = document.querySelector("#result")
-const btn = document.querySelector("button")
+//Correction Ex1 
+//const allInput = document.querySelectorAll('input')
+// const result = document.querySelector("#result")
+// const btn = document.querySelector("button")
 
-btn.addEventListener('click', function() {
-    let somme = 0
-    for(let i=0; i < allInput.length; i++) {
-        somme += parseFloat(allInput[i].value)
+// btn.addEventListener('click', function() {
+//     let somme = 0
+//     for(let i=0; i < allInput.length; i++) {
+//         somme += parseFloat(allInput[i].value)
+//     }
+//     result.innerText = "Le resultat est de "+ somme
+// })
+
+const input = document.querySelector('input')
+const result = document.querySelector("#result")
+
+input.addEventListener('keyup', function(e) {
+    let age = parseInt(e.target.value)
+    if(age > 0 && age < 130) {
+        result.innerHTML = "Age correct"
     }
-    result.innerText = "Le resultat est de "+ somme
+    else {
+        result.innerHTML = "Age incorrect, merci de ressayer"
+    }
 })
