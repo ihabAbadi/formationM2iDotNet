e520b87b3051a434b6e8fc6bc7ce0a93fc8da374
ihab@utopios.net <ihab@utopios.net>
2020-06-26 11:54:22 +0200

    correction morpion v2

--- coursJavaScript/introMorpion.js
+++ coursJavaScript/introMorpion.js
@@ -1,4 +1,4 @@
-const taille = 4
+const taille = 3
 const grid = document.querySelector(".grid")
 
 for(let i=1; i <= taille; i++) {
@@ -11,5 +11,27 @@ grid.addEventListener('click', function(e) {
     if(e.target.innerText == "") {
         e.target.innerText = (isFirstPlayer) ? 'X' : 'O'
         isFirstPlayer = !isFirstPlayer
+        if(testWin()){
+            alert("Vous avez gagné")
+        }
     }
 })
+
+function testWin() {
+    let win = false
+    for(let i=1; i <= taille; i++) {
+        const elementLigne = document.querySelectorAll('div[data-x="'+i+'"]')
+        let sameValue = true
+        for(let k=0; k < elementLigne.length-1; k++) {
+            if( elementLigne[k].innerText== "" || elementLigne[k].innerText != elementLigne[k+1].innerText) {
+                sameValue = false
+                break;
+            }
+        }
+        if(sameValue) {
+            win = true
+            break
+        }
+    }
+    return win
+}
