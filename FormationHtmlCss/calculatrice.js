bb53dbfe25aa2034aa2a69350a1db00681d2293d
ihab@utopios.net <ihab@utopios.net>
2020-06-29 09:22:04 +0200

    correction calculatrice

--- FormationHtmlCss/calculatrice.js
+++ FormationHtmlCss/calculatrice.js
@@ -3,10 +3,10 @@ const header = document.querySelector('header')
 let firstNumber = true
 let operationCours = undefined
 let nombre
-standard.addEventListener('click', function(e) {
+standard.addEventListener('click', function (e) {
     const valueButton = e.target.innerText
-    if(!isNaN(valueButton)) {
-        if(firstNumber) {
+    if (!isNaN(valueButton)) {
+        if (firstNumber) {
             header.innerText = valueButton
             firstNumber = false
         }
@@ -16,14 +16,26 @@ standard.addEventListener('click', function(e) {
     }
     else {
         firstNumber = true
-        switch(valueButton) {
+        switch (valueButton) {
             case "+":
-                if(operationCours == undefined) {
+                if (operationCours == undefined) {
                     nombre = parseFloat(header.innerText)
                 }
                 else {
-                    switch(operationCours) {
+                    switch (operationCours) {
                         case "+":
+                            nombre = nombre + parseFloat(header.innerText)
+                            header.innerText = nombre
+                            break;
+                        case "-":
+                            nombre = nombre - parseFloat(header.innerText)
+                            header.innerText = nombre
+                            break;
+                        case "X":
+                            nombre = nombre * parseFloat(header.innerText)
+                            header.innerText = nombre
+                            break;
+                        case "/":
                             nombre = nombre + parseFloat(header.innerText)
                             header.innerText = nombre
                             break;
@@ -31,6 +43,119 @@ standard.addEventListener('click', function(e) {
                 }
                 operationCours = "+"
                 break;
+            case "-":
+                if (operationCours == undefined) {
+                    nombre = parseFloat(header.innerText)
+                }
+                else {
+                    switch (operationCours) {
+                        case "+":
+                            nombre = nombre + parseFloat(header.innerText)
+                            header.innerText = nombre
+                            break;
+                        case "-":
+                            nombre = nombre - parseFloat(header.innerText)
+                            header.innerText = nombre
+                            break;
+                        case "X":
+                            nombre = nombre * parseFloat(header.innerText)
+                            header.innerText = nombre
+                            break;
+                        case "/":
+                            nombre = nombre + parseFloat(header.innerText)
+                            header.innerText = nombre
+                            break;
+                    }
+                }
+                operationCours = "-"
+                break;
+            case "X":
+                if (operationCours == undefined) {
+                    nombre = parseFloat(header.innerText)
+                }
+                else {
+                    switch (operationCours) {
+                        case "+":
+                            nombre = nombre + parseFloat(header.innerText)
+                            header.innerText = nombre
+                            break;
+                        case "-":
+                            nombre = nombre - parseFloat(header.innerText)
+                            header.innerText = nombre
+                            break;
+                        case "X":
+                            nombre = nombre * parseFloat(header.innerText)
+                            header.innerText = nombre
+                            break;
+                        case "/":
+                            nombre = nombre + parseFloat(header.innerText)
+                            header.innerText = nombre
+                            break;
+                    }
+                }
+                operationCours = "X"
+                break;
+            case "/":
+                if (operationCours == undefined) {
+                    nombre = parseFloat(header.innerText)
+                }
+                else {
+                    switch (operationCours) {
+                        case "+":
+                            nombre = nombre + parseFloat(header.innerText)
+                            header.innerText = nombre
+                            break;
+                        case "-":
+                            nombre = nombre - parseFloat(header.innerText)
+                            header.innerText = nombre
+                            break;
+                        case "X":
+                            nombre = nombre * parseFloat(header.innerText)
+                            header.innerText = nombre
+                            break;
+                        case "/":
+                            nombre = nombre + parseFloat(header.innerText)
+                            header.innerText = nombre
+                            break;
+                    }
+                }
+                operationCours = "/"
+                break;
+            case "=":
+                switch (operationCours) {
+                    case "+":
+                        nombre = nombre + parseFloat(header.innerText)
+                        header.innerText = nombre
+                        break;
+                    case "-":
+                        nombre = nombre - parseFloat(header.innerText)
+                        header.innerText = nombre
+                        break;
+                    case "X":
+                        nombre = nombre * parseFloat(header.innerText)
+                        header.innerText = nombre
+                        break;
+                    case "/":
+                        nombre = nombre + parseFloat(header.innerText)
+                        header.innerText = nombre
+                        break;
+                }
+                operationCours = undefined
+                break;
+            case ",":
+                if (header.innerText.indexOf('.') == -1) {
+                    header.innerText += '.'
+                    firstNumber = false
+                }
+                break;
+            case "C":
+                header.innerText = "0"
+                operationCours = undefined
+                firstNumber = true
+                break;
+            case "+/-":
+                header.innerText = parseFloat(header.innerText) * -1
+            break;
         }
     }
 })
