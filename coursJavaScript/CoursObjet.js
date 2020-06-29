82337aa560ce4aa4d3311b8c11c5ef900f1b8fb2
ihab@utopios.net <ihab@utopios.net>
2020-06-29 11:32:51 +0200

    correction voiture

--- coursJavaScript/CoursObjet.js
+++ coursJavaScript/CoursObjet.js
@@ -37,20 +37,53 @@
 
 // fonctionObject()
 
-function personne(nom, prenom) {
-    this.nom = nom;
-    this.prenom = prenom;
-    this.afficher = function() {
-        alert(this.nom + " "+this.prenom)
-    }
-}
+// function personne(nom, prenom) {
+//     this.nom = nom;
+//     this.prenom = prenom;
+//     this.afficher = function() {
+//         alert(this.nom + " "+this.prenom)
+//     }
+// }
 
-const p1 = new personne("abadi", "ihab")
-const p2 = new personne("titi", "minet")
+// const p1 = new personne("abadi", "ihab")
+// const p2 = new personne("titi", "minet")
 // alert(p1.nom + " "+p1.prenom)
 // alert(p2.nom + " "+p2.prenom)
 // p1.afficher()
 // p2.afficher()
 
-console.log(p1)
-console.log(p2)
+// console.log(p1)
+// console.log(p2)
+
+function voiture(model, marque, immat, conso) {
+    this.model = model
+    this.marque = marque
+    this.immat = immat
+    this.reservoir = 0
+    this.conso = conso
+    this.afficher = function() {
+        console.log("Voiture model : "+ this.model + ", marque : "+this.marque + ", immat : "+this.immat+", reservoir : "+this.reservoir)
+    }
+    this.fairePlein = function(nombreLitre) {
+        this.reservoir += nombreLitre
+    }
+    this.rouler = function(nombreKm) {
+        const consoF = nombreKm * this.conso / 100
+        if(this.reservoir - consoF >= 0) {
+            console.log("Voiture roule ")
+            this.reservoir -= consoF
+        }
+        else {
+            console.log("Merci de faire le plein il vous manque : "+ (consoF - this.reservoir) + " L")
+        }
+    }
+}
+const v1 = new voiture("kuga" ,"Ford", "123456",9)
+v1.afficher()
+v1.fairePlein(50)
+v1.afficher()
+v1.rouler(30)
+v1.afficher()
+v1.rouler(1000)
+const v2 = new voiture("Kia", "ceed", "233333", 7)
+
