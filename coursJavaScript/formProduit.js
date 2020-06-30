a025f3884fb324bfd1a429809b092a97ce987b1f
ihab@utopios.net <ihab@utopios.net>
2020-06-30 16:10:11 +0200

    correction formulaire version objet

--- coursJavaScript/formProduit.js
+++ coursJavaScript/formProduit.js
@@ -1,57 +1,169 @@
+
+// let errors = []
+
+// const produits = []
+// formProduit.addEventListener('keydown', function (e) {
+//     e.target.classList.remove("border-danger")
+// })
+// formProduit.addEventListener('submit', function (e) {
+//     e.preventDefault()
+//     let error = false
+//     errors = []
+//     // document.querySelector('input[name="titre"]').classList.remove("border-danger")
+//     // document.querySelector('input[name="prix"]').classList.remove("border-danger")
+//     // document.querySelector('textarea').classList.remove("border-danger")
+//     const titre = document.querySelector('input[name="titre"]').value
+//     if (titre == "") {
+//         error = true
+//         errors.push("Merci d'ajouter un titre du produit")
+//         document.querySelector('input[name="titre"]').classList.add("border-danger")
+//     }
+//     const prix = document.querySelector('input[name="prix"]').value
+//     if (prix == "" || isNaN(prix)) {
+//         error = true
+//         errors.push("Merci d'ajouter un prix du produit")
+//         document.querySelector('input[name="prix"]').classList.add("border-danger")
+//     }
+
+//     const description = document.querySelector("textarea").value
+//     if (description == "") {
+//         error = true
+//         errors.push("Merci d'ajouter un description du produit")
+//         document.querySelector("textarea").classList.add("border-danger")
+//     }
+//     if (!error) {
+//         produits.push({
+//             id: (produits[produits.length - 1] != undefined) ? (produits[produits.length - 1].id + 1) : 1,
+//             titre: titre,
+//             prix: prix,
+//             description: description
+//         })
+
+//         let produit = ""
+//         produit += "<div class='col'>"
+//         produit += "<div class='row'><div class='col'>" + titre + "</div></div>"
+//         produit += "<div class='row'><div class='col'>" + prix + "</div></div>"
+//         produit += "<div class='row'><div class='col'>" + description + "</div></div>"
+//         produit += "</div>"
+//         affichage.innerHTML += produit
+//         document.querySelector('input[name="prix"]').value = ""
+//         document.querySelector('input[name="titre"]').value = ""
+//         document.querySelector('textarea').value = ""
+//     }
+//     else {
+
+//     }
+// })
+
+class GestionProduits {
+    
+    //Constructeur des objets de types gestion produits
+    constructor(formulaire, affichageHtml) {
+        
+        //Attribut pour stocker les produits
+        this.produits = []
+
+        //Attribut pour le formulaire
+        this.formulaire = formulaire
+
+        //Attribut pour la zone d'affichage html
+        this.affichageHtml = affichageHtml
+        this.errors = []
+    }
+
+
+    //Methode pour ajouter un produit dans l'attribut produits à partir du formulaire
+    ajouterProduit() {
+        let error = false
+        const titre = this.formulaire.querySelector('input[name="titre"]').value
+        const prix = this.formulaire.querySelector('input[name="prix"]').value
+        const description = this.formulaire.querySelector('textarea').value
+        if (titre == "") {
+            error = true
+            //errors.push("Merci d'ajouter un titre du produit")
+            this.formulaire.querySelector('input[name="titre"]').classList.add("border-danger")
+        }
+        if (prix == "" || isNaN(prix)) {
+            error = true
+            //errors.push("Merci d'ajouter un prix du produit")
+            this.formulaire.querySelector('input[name="prix"]').classList.add("border-danger")
+        }
+        if (description == "") {
+            error = true
+            //errors.push("Merci d'ajouter un description du produit")
+            this.formulaire.querySelector("textarea").classList.add("border-danger")
+        }
+        if (!error) {
+            this.produits.push({
+                id: (this.produits[this.produits.length - 1] != undefined) ? (this.produits[this.produits.length - 1].id + 1) : 1,
+                titre: titre,
+                prix: prix,
+                description: description
+            })
+            this.affichageHtmlProduits()
+            this.clearForm()
+        }
+        
+    }
+
+    //Méthode pour afficher les produits de l'attribut produits dans la zone html
+    affichageHtmlProduits() {
+        this.affichageHtml.innerHTML = ""
+        for (let produit of this.produits) {
+            let affichageProduit = "";
+            affichageProduit += "<div class='col'>"
+            affichageProduit += "<div class='row'><div class='col'>" + produit.titre + "</div></div>"
+            affichageProduit += "<div class='row'><div class='col'>" + produit.prix + "</div></div>"
+            affichageProduit += "<div class='row'><div class='col'>" + produit.description + "</div></div>"
+            affichageProduit += "<div class='row'><button class='col edit btn btn-warning' data-id='"+produit.id+"'>Modifier</button></div>"
+            affichageProduit += "<div class='row'><button class='col delete btn btn-danger' data-id='"+produit.id+"'>Supprimer</button></div>"
+            affichageProduit += "</div>"
+            this.affichageHtml.innerHTML += affichageProduit
+        }
+    }
+
+    //Méthode pour vider les champs du formulaire
+    clearForm() {
+        this.formulaire.querySelector('input[name="titre"]').value = ""
+        this.formulaire.querySelector('input[name="prix"]').value = ""
+        this.formulaire.querySelector('textarea').value = ""
+    }
+
+    //Méthode pour écouter les evenements du formulaires (event submit pour valider le formulaire, supprimer les bordures rouges)
+    eventListener() {
+        this.formulaire.addEventListener('submit', (e) => {
+            e.preventDefault()
+            this.ajouterProduit()
+        })
+        this.formulaire.addEventListener('keydown', (e) => {
+            e.target.classList.remove("border-danger")
+        })
+
+        this.affichageHtml.addEventListener('click', (e) => {
+            if(e.target.classList.contains("delete")) {
+                this.supprimerProduit(e.target.getAttribute("data-id"))
+            }
+            else if(e.target.classList.contains("edit")) {
+                this.modifierProduit(e.target.getAttribute("data-id"))
+            }
+        })
+    }
+
+    //Méthode pour supprimer un produit avec son id
+    supprimerProduit(id) {
+        //Insctruction pour la suppression du produit
+        alert("suppression produit "+id)
+        this.affichageHtmlProduits()
+    }
+
+    //Méthode modification produit
+    modifierProduit(id) {
+        //Instruction pour la modification du produit
+        alert("modification produit "+id)
+        this.affichageHtmlProduits()
+    }
+}
 const formProduit = document.querySelector("#formProduit")
 const affichage = document.querySelector("#affichage")
-let errors = []
-
-const produits = []
-formProduit.addEventListener('keydown', function(e) {
-    e.target.classList.remove("border-danger")
-})
-formProduit.addEventListener('submit', function(e) {
-    e.preventDefault()
-    let error = false
-    errors = []
-    // document.querySelector('input[name="titre"]').classList.remove("border-danger")
-    // document.querySelector('input[name="prix"]').classList.remove("border-danger")
-    // document.querySelector('textarea').classList.remove("border-danger")
-    const titre = document.querySelector('input[name="titre"]').value
-    if(titre == "") {
-        error = true
-        errors.push("Merci d'ajouter un titre du produit")
-        document.querySelector('input[name="titre"]').classList.add("border-danger")
-    }
-    const prix = document.querySelector('input[name="prix"]').value
-    if(prix == "" || isNaN(prix)) {
-        error = true
-        errors.push("Merci d'ajouter un prix du produit")
-        document.querySelector('input[name="prix"]').classList.add("border-danger")    
-    }
-    
-    const description = document.querySelector("textarea").value
-    if(description == "") {
-        error = true
-        errors.push("Merci d'ajouter un description du produit")
-        document.querySelector("textarea").classList.add("border-danger")
-    }
-    if(!error) {
-        produits.push({
-            id : (produits[produits.length-1]!= undefined) ? (produits[produits.length-1].id+1) : 1,
-            titre : titre,
-            prix : prix,
-            description : description
-        })
-        
-        let produit = ""
-        produit += "<div class='col'>"
-        produit += "<div class='row'><div class='col'>"+titre+"</div></div>"
-        produit += "<div class='row'><div class='col'>"+prix+"</div></div>"
-        produit += "<div class='row'><div class='col'>"+description+"</div></div>"
-        produit += "</div>"
-        affichage.innerHTML += produit
-        document.querySelector('input[name="prix"]').value = ""
-        document.querySelector('input[name="titre"]').value = ""
-        document.querySelector('textarea').value = ""
-    }
-    else {
-
-    }
-})
+const gestion = new GestionProduits(formProduit, affichage)
+gestion.eventListener()
