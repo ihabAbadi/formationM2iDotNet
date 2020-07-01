3e742538ab5419e41673755272097ea23367f00b
ihab@utopios.net <ihab@utopios.net>
2020-07-01 17:10:54 +0200

    correction snake partie 1

--- canvas.js
+++ canvas.js
@@ -16,65 +16,112 @@ let ctx = canvas.getContext("2d")
 // //effacer la totalité du canvas
 // ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-let dx = 2
-let dy = 2
+// let dx = 2
+// let dy = 2
+// let x = 10
+// let y = 20
+// let xr = 50
+// let yr = 70
+// var speed = 20
+// let anim = () => {
+//     x = x + dx
+//     y = y + dy
+//     // xr += dx
+//     // yr += dy
+//     //déplacer le cercle dans le sens inverse quand on arrive a la fin du canvas => largeur
+//     if (x >= (canvas.width - 15)) {
+//         dx = -dx
+//     }
+//     //Inverser encore le sens quand on arrive au début du canvas => largeur
+//     else if (x <= 15) {
+//         dx = 2
+//     }
+//     //hauteur
+//     if (y >= (canvas.height - 15)) {
+//         dy = -dy
+
+//     }
+//     //hauteur
+//     else if (y <= 15) {
+//         dy = 2
+
+//     }
+
+//     ctx.clearRect(0, 0, canvas.width, canvas.height)
+//     ctx.beginPath()
+
+//     ctx.arc(x, y, 15, 0, Math.PI * 2)
+//     //ctx.fillStyle = "blue"
+//     //ctx.fill()
+//     ctx.strokeStyle = "blue"
+//     ctx.stroke()
+//     ctx.closePath()
+
+// }
+
+// setInterval(() => {
+//     anim()
+// }, speed);
+
+// document.addEventListener('keyup',(e) => {
+//     if(e.keyCode == 39) {
+//         dx = 2
+//     }
+//     else if(e.keyCode == 37) {
+//         dx = -2
+//     }
+//     else if(e.keyCode == 40) {
+//         dy = 2
+//     }
+//     else if(e.keyCode == 38) {
+//         dy = -2
+//     }
+
+// })
+const delta = 2
 let x = 10
-let y = 20
-let xr = 50
-let yr = 70
-var speed = 20
-let anim = () => {
-    x = x + dx
-    y = y + dy
-    // xr += dx
-    // yr += dy
-    //déplacer le cercle dans le sens inverse quand on arrive a la fin du canvas => largeur
-    if (x >= (canvas.width - 15)) {
-        dx = -dx
-    }
-    //Inverser encore le sens quand on arrive au début du canvas => largeur
-    else if (x <= 15) {
-        dx = 2
-    }
-    //hauteur
-    if (y >= (canvas.height - 15)) {
-        dy = -dy
-       
-    }
-    //hauteur
-    else if (y <= 15) {
-        dy = 2
-        
-    }
-    
+let y = 10
+let dx = 0
+let dy = 0
+let gameOver = false
+const width = 50
+const height = 50
+const drawRect = (i, j) => {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.beginPath()
-
-    ctx.arc(x, y, 15, 0, Math.PI * 2)
-    //ctx.fillStyle = "blue"
-    //ctx.fill()
-    ctx.strokeStyle = "blue"
-    ctx.stroke()
+    ctx.rect(i, j, width, height)
+    ctx.fillStyle = "#cd2127"
+    ctx.fill()
     ctx.closePath()
-    
 }
-
 setInterval(() => {
-    anim()
-}, speed);
-
-document.addEventListener('keyup',(e) => {
-    if(e.keyCode == 39) {
-        dx = 2
+    if (!gameOver) {
+        x += dx
+        y += dy
+        if (x >= canvas.width - width || x <= 0 || y >= canvas.height - height || y <= 0) {
+            alert("game over")
+            gameOver = true
+        }
+        else {
+            drawRect(x, y)
+        }
     }
-    else if(e.keyCode == 37) {
-        dx = -2
+}, 10);
+document.addEventListener('keyup', (e) => {
+    if (e.keyCode == 37) {
+        dy = 0
+        dx = -delta
     }
-    else if(e.keyCode == 40) {
-        dy = 2
+    else if (e.keyCode == 39) {
+        dy = 0
+        dx = delta
     }
-    else if(e.keyCode == 38) {
-        dy = -2
+    else if (e.keyCode == 38) {
+        dx = 0
+        dy = -delta
     }
-    
+    else if (e.keyCode == 40) {
+        dx = 0
+        dy = delta
+    }
 })
