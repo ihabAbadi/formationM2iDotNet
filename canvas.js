fb4f0d17857bff02731c39982700788cf67e534f
ihab@utopios.net <ihab@utopios.net>
2020-07-01 16:08:48 +0200

    utilisation  des canvas

---
+++ canvas.js
@@ -0,0 +1,80 @@
+let canvas = document.querySelector('canvas')
+let ctx = canvas.getContext("2d")
+// ctx.beginPath()
+// ctx.rect(10, 100, 50, 80)
+// ctx.fillStyle = "#cd2127"
+// ctx.fill()
+// ctx.closePath()
+
+// ctx.beginPath()
+// ctx.arc(50, 80, 15, 0, Math.PI * 2)
+// //ctx.fillStyle = "blue"
+// //ctx.fill()
+// ctx.strokeStyle = "blue"
+// ctx.stroke()
+// ctx.closePath()
+// //effacer la totalité du canvas
+// ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+let dx = 2
+let dy = 2
+let x = 10
+let y = 20
+let xr = 50
+let yr = 70
+var speed = 20
+let anim = () => {
+    x = x + dx
+    y = y + dy
+    // xr += dx
+    // yr += dy
+    //déplacer le cercle dans le sens inverse quand on arrive a la fin du canvas => largeur
+    if (x >= (canvas.width - 15)) {
+        dx = -dx
+    }
+    //Inverser encore le sens quand on arrive au début du canvas => largeur
+    else if (x <= 15) {
+        dx = 2
+    }
+    //hauteur
+    if (y >= (canvas.height - 15)) {
+        dy = -dy
+       
+    }
+    //hauteur
+    else if (y <= 15) {
+        dy = 2
+        
+    }
+    
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.beginPath()
+
+    ctx.arc(x, y, 15, 0, Math.PI * 2)
+    //ctx.fillStyle = "blue"
+    //ctx.fill()
+    ctx.strokeStyle = "blue"
+    ctx.stroke()
+    ctx.closePath()
+    
+}
+
+setInterval(() => {
+    anim()
+}, speed);
+
+document.addEventListener('keyup',(e) => {
+    if(e.keyCode == 39) {
+        dx = 2
+    }
+    else if(e.keyCode == 37) {
+        dx = -2
+    }
+    else if(e.keyCode == 40) {
+        dy = 2
+    }
+    else if(e.keyCode == 38) {
+        dy = -2
+    }
+    
+})
