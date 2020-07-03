246b26f6961befee9fc38ebedea34fd6848ff198
ihab@utopios.net <ihab@utopios.net>
2020-07-03 10:37:58 +0200

    event et state react

--- coursReact/first-react-app/src/Products/ListProducts.js
+++ coursReact/first-react-app/src/Products/ListProducts.js
@@ -5,9 +5,9 @@ export class ListProduct extends Component {
     constructor(props) {
         super(props)
         this.products= [
-            {title:'produit1', content : 'contenu produit 1', price:10, images : ['https://picsum.photos/200','https://picsum.photos/200','https://picsum.photos/200']},
-            {title:'produit2', content : 'contenu produit 2', price:1000, images : ['https://picsum.photos/200','https://picsum.photos/200','https://picsum.photos/200']},
-            {title:'produit3', content : 'contenu produit 3', price:1000, images : ['https://picsum.photos/200','https://picsum.photos/200','https://picsum.photos/200']},
+            {title:'produit1', content : 'contenu produit 1', price:10, images : ['https://picsum.photos/id/1002/200','https://picsum.photos/id/1005/200','https://picsum.photos/id/1010/200']},
+            {title:'produit2', content : 'contenu produit 2', price:1000, images : ['https://picsum.photos/id/1003/200','https://picsum.photos/id/1006/200','https://picsum.photos/id/1011/200']},
+            {title:'produit3', content : 'contenu produit 3', price:1000, images : ['https://picsum.photos/id/1004/200','https://picsum.photos/id/1012/200','https://picsum.photos/id/1008/200']},
         ]
     }
 
--- coursReact/first-react-app/src/Products/Product.js
+++ coursReact/first-react-app/src/Products/Product.js
@@ -4,8 +4,28 @@ import { Images } from "./Images"
 export class Product extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            qty : 1,
+            total : this.props.price * 1
+        }
     }
 
+    // clickProduit = (e) => {
+    //     //alert(e.target.innerHTML)
+    //     alert("je viens de cliquer sur "+this.props.title)
+    // }
+    more = () => {
+        this.setState({
+            qty : this.state.qty+1,
+            total : this.props.price * (this.state.qty+1)
+        })
+    }
+    less = () => {
+        this.setState({
+            qty : this.state.qty-1,
+            total : this.props.price * (this.state.qty-1)
+        })
+    }
     render() {
         return (
             <div>
@@ -17,8 +37,20 @@ export class Product extends Component {
                     {this.props.price}
                 </div>
                 <div>
+                    Qty : {this.state.qty}
+                </div>
+                <div>
+                    Total : {this.state.total}
+                </div>
+                <div>
                     <Images uri={this.props.images}></Images>
                 </div>
+                <div>
+                <button onClick={this.less}>moin</button><button onClick={this.more}>Plus</button>
+                </div>
+                {/* <div onClick={this.clickProduit}>
+                    Je clique sur le produit
+                </div> */}
             </div>
         )
     }
