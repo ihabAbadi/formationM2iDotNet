07ead0487c323b8ce6823d87ac2ed69d309077cf
ihab@utopios.net <ihab@utopios.net>
2020-07-03 09:39:38 +0200

    correction component product avec map

--- coursReact/first-react-app/src/Products/ListProducts.js
+++ coursReact/first-react-app/src/Products/ListProducts.js
@@ -4,14 +4,23 @@ import { Product } from "./Product"
 export class ListProduct extends Component {
     constructor(props) {
         super(props)
+        this.products= [
+            {title:'produit1', content : 'contenu produit 1', price:10, image : 'https://img.phonandroid.com/2020/06/concept-iphone-douze.jpg'},
+            {title:'produit2', content : 'contenu produit 2', price:1000, image : 'https://img.phonandroid.com/2020/06/concept-iphone-douze.jpg'},
+            {title:'produit3', content : 'contenu produit 3', price:1000, image : 'https://img.phonandroid.com/2020/06/concept-iphone-douze.jpg'},
+            {title:'produit3', content : 'contenu produit 3', price:1000, image : 'https://img.phonandroid.com/2020/06/concept-iphone-douze.jpg'},
+        ]
     }
 
     render() {
         return(
             <div>
                 <h1>Liste des produits</h1>
-                <Product title="produit 1" content="content produit 1" price="10" image="https://img.phonandroid.com/2020/06/concept-iphone-douze.jpg"></Product>
-                <Product title="produit 2" content="content produit 2" price="1000" image="https://img.phonandroid.com/2020/06/concept-iphone-douze.jpg"></Product>
+                {this.products.map((element) => {
+                    return(
+                        <Product title={element.title} content={element.content} price={element.price} image={element.image} />
+                    )
+                })}
             </div>
         )
     }
