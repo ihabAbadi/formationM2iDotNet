e2639fe9cf010c7e9c945eab80359e3889e6d1a9
ihab@utopios.net <ihab@utopios.net>
2020-07-06 09:20:02 +0200

    correction form product avec image

--- coursReact/first-react-app/src/Products/FormProduct.js
+++ coursReact/first-react-app/src/Products/FormProduct.js
@@ -7,7 +7,8 @@ export class FormProduct extends Component {
             product : {
                 images : []
             },
-            numberImageInput : 1
+            numberImageInput : 1,
+            tmpImage : undefined
         }
     }
 
@@ -27,11 +28,34 @@ export class FormProduct extends Component {
     submitForm = (e) => {
         e.preventDefault()
         this.props.addProduct(this.state.product)
+        this.setState({
+            numberImageInput : 1
+        })
+    }
+
+    changeImage = (e) => {
+        this.setState({
+            tmpImage : e.target.value
+        })
+    }
+
+    validImage = () => {
+        if(this.state.tmpImage !== undefined) {
+            let p = {...this.state.product}
+            p['images'].push(this.state.tmpImage)
+            this.setState({
+                product : p,
+                tmpImage:undefined
+            })
+        }
+        this.setState({
+            numberImageInput:this.state.numberImageInput+1
+        })
     }
     render() {
         const inputs = []
         for(let i=1; i <= this.state.numberImageInput; i++ ){
-            inputs.push(<div><input type='text' placeholder="Url Image" /></div>)
+            inputs.push(<div><input  type='text' onChange={this.changeImage} placeholder="Url Image" /></div>)
         }
         return(
             <div>
@@ -45,11 +69,7 @@ export class FormProduct extends Component {
                     <div>
                         <input type="text" onChange={this.onChangeField} name="price" placeholder="Prix  du produit" />
                     </div>
-                    <div><button onClick={()=>{
-                        this.setState({
-                            numberImageInput:this.state.numberImageInput+1
-                        })
-                    }}>Ajouter image</button></div>
+                    <div><a onClick={this.validImage}>Ajouter image</a></div>
                     {inputs}
                     <div>
                         <button type="submit">Valider</button>
