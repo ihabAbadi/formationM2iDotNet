13e532cba04df9dce591d336004b13c58ef2bc04
ihab@utopios.net <ihab@utopios.net>
2020-07-10 11:28:13 +0200

    read and write file on web server

--- serveurNodeJs/index.js
+++ serveurNodeJs/index.js
@@ -4,16 +4,18 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
 
+const fs = require('fs')
+
 const data = [
     {nom : 'toto', prenom : 'tata'},
     {nom : 'minet', prenom : 'titi'},
     {nom : 'toto', prenom : 'tata'},
 ]
-const annonces = [
-    {title : 'annonce 1', description : 'contenu annonc 1', images : []},
-    {title : 'annonce 2', description : 'contenu annonc 2', images : []},
-    {title : 'annonce 3', description : 'contenu annonc 3', images : []},
-]
+// const annonces = [
+//     {title : 'annonce 1', description : 'contenu annonc 1', images : []},
+//     {title : 'annonce 2', description : 'contenu annonc 2', images : []},
+//     {title : 'annonce 3', description : 'contenu annonc 3', images : []},
+// ]
 
 //Autoriser les requetes quelque soit l'origin
 app.use((req,res,next) => {
@@ -36,12 +38,18 @@ app.get('/personne',(req,res) => {
 
 app.post('/addAnnonce', (req, res) => {
     console.log(req.body)
+    const contenuFichierAnnonces = fs.readFileSync('annonces.json')
+    //JSON.parse => convertir une chaine de caractère en objet json quand c'est possible
+    const annonces = JSON.parse(contenuFichierAnnonces)
     annonces.push(req.body)
+    //On ecrit des chaines de caractères dans notre fichier annonces.json => JSON.stringify pôur convertir un objet en chaine de caractère
+    fs.writeFileSync('annonces.json', JSON.stringify(annonces))
     res.json({msg : 'annonce ajoutée'})
 })
 
 app.get('/annonces', (req, res) => {
-    
+    const contenuFichierAnnonces = fs.readFileSync('annonces.json')
+    const annonces = JSON.parse(contenuFichierAnnonces)
     res.json(annonces)
 })
 
