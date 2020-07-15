ec5b06c46cffe4ef7e0cce17c790741971efc594
ihab@utopios.net <ihab@utopios.net>
2020-07-15 10:20:08 +0200

    utilisation du hook useState dans contacts

--- coursReact/contacts/src/contacts/Contact.js
+++ coursReact/contacts/src/contacts/Contact.js
@@ -1,23 +1,39 @@
 import React, { Component } from "react"
 
-export class Contact extends Component {
-    
-    constructor(props) {
-        super(props)
-    }
+// export class Contact extends Component {
 
-    render() {
-        return(
-            <div className="row">
-                <div className="col-11">
-                    {this.props.name}
-                </div>
-                <div onClick={()=> {
-                    this.props.deleteContact(this.props.name)
-                }} className="col-1">
-                    X
-                </div>
+//     constructor(props) {
+//         super(props)
+//     }
+
+//     render() {
+//         return(
+//             <div className="row">
+//                 <div className="col-11">
+//                     {this.props.name}
+//                 </div>
+//                 <div onClick={()=> {
+//                     this.props.deleteContact(this.props.name)
+//                 }} className="col-1">
+//                     X
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+export const Contact = (props) => {
+
+    return (
+        <div className="row">
+            <div className="col-11">
+                {props.name}
             </div>
-        )
-    }
+            <div onClick={() => {
+                props.deleteContact(props.name)
+            }} className="col-1">
+                X
+                </div>
+        </div>
+    )
 }
--- coursReact/contacts/src/contacts/FormContact.js
+++ coursReact/contacts/src/contacts/FormContact.js
@@ -1,33 +1,56 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 
-export class FormContact extends Component {
+// export class FormContact extends Component {
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            contact :undefined
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             contact :undefined
+//         }
+//     }
+
+//     addContact = () => {
+//         if(this.state.contact != undefined) {
+//             this.props.addContact(this.state.contact)
+//         }
+//     }
+//     render() {
+//         return (
+//             <div className="container">
+//                 <div className="row justify-content-center">
+//                     <input type="text" onChange={(e) => {
+//                         this.setState({
+//                             contact: e.target.value
+//                         })
+//                     }} placeholder="Name of contact" className="form-control col" />
+//                 </div>
+//                 <div className="row justify-content-center">
+//                     <button onClick={this.addContact} className="form-control btn btn-primary">Add Contact</button>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+export const FormContact = (props) => {
+
+    const [contact, setContact] = useState(undefined)    
+    const addContact = (e) => {
+        if (contact != undefined) {
+            props.addContact(contact)
         }
     }
-
-    addContact = () => {
-        if(this.state.contact != undefined) {
-            this.props.addContact(this.state.contact)
-        }
-    }
-    render() {
-        return (
-            <div className="container">
-                <div className="row justify-content-center">
-                    <input type="text" onChange={(e) => {
-                        this.setState({
-                            contact: e.target.value
-                        })
-                    }} placeholder="Name of contact" className="form-control col" />
-                </div>
-                <div className="row justify-content-center">
-                    <button onClick={this.addContact} className="form-control btn btn-primary">Add Contact</button>
-                </div>
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <input type="text" onChange={(e) => {
+                    setContact(e.target.value)
+                }} placeholder="Name of contact" className="form-control col" />
             </div>
-        )
-    }
+            <div className="row justify-content-center">
+                <button onClick={addContact} className="form-control btn btn-primary">Add Contact</button>
+            </div>
+        </div>
+    )
 }
+
