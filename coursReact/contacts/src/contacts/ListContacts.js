07f93f61350be333f405942e31c4303db89795eb
ihab@utopios.net <ihab@utopios.net>
2020-07-15 10:42:44 +0200

    modification des contacts

--- coursReact/contacts/src/contacts/ListContacts.js
+++ coursReact/contacts/src/contacts/ListContacts.js
@@ -1,44 +1,76 @@
-import React, { Component } from "react"
+import React, { Component, useState } from "react"
 import { FormContact } from "./FormContact"
 import { Contact } from "./Contact"
 
-export class ListContacts extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            contacts : []
-        }
-    }
-    addContact = (contact) => {
-        let tmpContacts = [...this.state.contacts, contact]
-        this.setState({
-            contacts:tmpContacts
-        })
-    }
+// export class ListContacts extends Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             contacts : []
+//         }
+//     }
+//     addContact = (contact) => {
+//         let tmpContacts = [...this.state.contacts, contact]
+//         this.setState({
+//             contacts:tmpContacts
+//         })
+//     }
 
-    deleteContact = (contact) => {
+//     deleteContact = (contact) => {
+//         let tmpContacts = []
+//         for(let c of this.state.contacts) {
+//             if(c != contact) {
+//                 tmpContacts.push(c)
+//             }
+//         }
+//         this.setState({
+//             contacts: tmpContacts
+//         })
+//     }
+//     render() {
+//         return(
+//             <div>
+//                 <FormContact addContact={this.addContact}></FormContact>
+//                 <section className="container">
+//                     {this.state.contacts.map((c) => {
+//                         return(
+//                             <Contact deleteContact={this.deleteContact} name={c}></Contact>
+//                         )
+//                     })}
+//                 </section>
+//             </div>
+//         )
+//     }
+// }
+
+export const ListContacts = (props) => {
+
+    const [contacts, setContacts] = useState([])
+
+    const deleteContact = (contact) => {
         let tmpContacts = []
-        for(let c of this.state.contacts) {
+        for(let c of contacts) {
             if(c != contact) {
                 tmpContacts.push(c)
             }
         }
-        this.setState({
-            contacts: tmpContacts
-        })
+        setContacts(tmpContacts)
     }
-    render() {
-        return(
-            <div>
-                <FormContact addContact={this.addContact}></FormContact>
-                <section className="container">
-                    {this.state.contacts.map((c) => {
-                        return(
-                            <Contact deleteContact={this.deleteContact} name={c}></Contact>
-                        )
-                    })}
-                </section>
-            </div>
-        )
+
+    const addContact = (contact) => {
+        let tmpContacts = [...contacts, contact]
+        setContacts(tmpContacts)
     }
+    return (
+        <div>
+            <FormContact addContact={addContact}></FormContact>
+            <section className="container">
+                {contacts.map((c) => {
+                    return (
+                        <Contact deleteContact={deleteContact} name={c}></Contact>
+                    )
+                })}
+            </section>
+        </div>
+    )
 }
