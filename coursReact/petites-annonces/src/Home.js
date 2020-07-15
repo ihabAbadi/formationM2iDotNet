6e472cc3de689316bb5fb3086fc68fa458943029
ihab@utopios.net <ihab@utopios.net>
2020-07-15 14:12:00 +0200

    exemple d'utilisation du useEffect dans petite annonces

--- coursReact/petites-annonces/src/Home.js
+++ coursReact/petites-annonces/src/Home.js
@@ -1,57 +1,89 @@
-import React, { Component } from "react"
-import {DataService} from "./service/DataService"
+import React, { Component, useState, useEffect } from "react"
+import { DataService } from "./service/DataService"
 import Annonces from "./Annonce/Annonces"
 import Search from "./Search"
 // import axios from "axios"
-import {ApiService} from "./service/ApiService"
-class Home extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            annonces: []
-        }
-    }
+import { ApiService } from "./service/ApiService"
+// class Home extends Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             annonces: []
+//         }
+//     }
 
-    componentDidMount() {
-        console.log(this.props.favoris)
-        // this.setState({
-        //     annonces : (this.props.favoris != undefined && this.props.favoris == true) ? DataService.favorisAnnonces : DataService.annonces
-        // })
-        // axios.get('http://localhost:80/annonces').then(response=> {
-        //     console.log(response)
-        //     const res = response.data
-        //     this.setState({
-        //         annonces : res
-        //     })
-        // })
-        ApiService.get('annonces').then(response => {
-            this.setState({
-                annonces : response.data
-            })
-        })
-    }
+//     componentDidMount() {
+//         console.log(this.props.favoris)
+//         // this.setState({
+//         //     annonces : (this.props.favoris != undefined && this.props.favoris == true) ? DataService.favorisAnnonces : DataService.annonces
+//         // })
+//         // axios.get('http://localhost:80/annonces').then(response=> {
+//         //     console.log(response)
+//         //     const res = response.data
+//         //     this.setState({
+//         //         annonces : res
+//         //     })
+//         // })
+//         ApiService.get('annonces').then(response => {
+//             this.setState({
+//                 annonces : response.data
+//             })
+//         })
+//     }
 
 
-    search = (text) => {
+//     search = (text) => {
+//         let tmpAnnonces
+//         if(text == '' || text == undefined) {
+//             tmpAnnonces = DataService.annonces
+//         }
+//         else {
+//             tmpAnnonces =  DataService.annonces.filter(a=> (a.title.includes(text) || a.description.includes(text)))
+//         }
+//         this.setState({
+//             annonces: tmpAnnonces
+//         })
+//     }
+//     render() {
+//         return(
+//             <main >
+//                 <Search textSearch='Rechercher une annonce...' search={this.search}/>
+//                 <Annonces annonces={this.state.annonces}></Annonces>
+//             </main>
+//         )
+//     }
+// }
+
+const Home = (props) => {
+    const search = (text) => {
+        
         let tmpAnnonces
-        if(text == '' || text == undefined) {
+        if (text == '' || text == undefined) {
             tmpAnnonces = DataService.annonces
         }
         else {
-            tmpAnnonces =  DataService.annonces.filter(a=> (a.title.includes(text) || a.description.includes(text)))
+            tmpAnnonces = DataService.annonces.filter(a => (a.title.includes(text) || a.description.includes(text)))
         }
-        this.setState({
-            annonces: tmpAnnonces
+        setAnnonces(tmpAnnonces)
+    }
+
+    const [annonces, setAnnonces] = useState([])
+    const [loading,setLoading] = useState(false)
+    useEffect(() => {
+        ApiService.get('annonces').then(response => {
+            setAnnonces(response.data)
         })
-    }
-    render() {
-        return(
-            <main >
-                <Search textSearch='Rechercher une annonce...' search={this.search}/>
-                <Annonces annonces={this.state.annonces}></Annonces>
-            </main>
-        )
-    }
+    },[loading])
+
+    return (
+        <main >
+            <button onClick = {() => {
+                setLoading(!loading)
+            }}>Reload</button>
+            <Search textSearch='Rechercher une annonce...' search={search} />
+            <Annonces annonces={annonces}></Annonces>
+        </main>
+    )
 }
 
 export default Home
