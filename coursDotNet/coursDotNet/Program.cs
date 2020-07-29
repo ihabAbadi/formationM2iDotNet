b766b5a5b60b44fd24c39234fe916d19982f5d0b
ihab@utopios.net <ihab@utopios.net>
2020-07-29 09:30:32 +0200

    correction ex 1

--- coursDotNet/coursDotNet/Program.cs
+++ coursDotNet/coursDotNet/Program.cs
@@ -32,7 +32,7 @@ namespace coursDotNet
             //int c = 10;
             //Console.WriteLine(a);
             //Operation sur les variables 
-            int a = 35;
+            /*int a = 35;
             int b = 11;
             double res = (double)a / b;
             a += 34;
@@ -40,7 +40,73 @@ namespace coursDotNet
             //int t = a++;
             int t = ++a;
             Console.WriteLine(t);
-            Console.WriteLine(a);
+            Console.WriteLine(a);*/
+            //Structure conditionnelle
+            //if else
+            //int age = 32;
+            //Opérateur de comparaison
+            // == -> égalité, != -> diff, >=, <=, >, <
+            /*if(age >= 18)
+            {
+                Console.WriteLine("Majeur");
+            }
+            else
+            {
+                Console.WriteLine("Mineur");
+            }*/
+            //opérateur logique
+            // && => ET, || => OU, ! => Négation
+            //ternaire
+            /*string message;
+            if (age >= 18)
+            {
+                message = "majeur";
+            }
+            else
+            {
+                message = "mineur";
+            }*/
+            //string message = (age >= 18) ? "majeur" : "mineur";
+            //Instruction lecture, Ecriture dans une console.
+            //Console.WriteLine("test");
+            //Console.Write("Votre nom : ");
+            //string nom = Console.ReadLine();
+            //Console.Write("Votre age : ");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(age);
+            //Ex 1
+            Console.WriteLine("Saisir AB");
+            double ab = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Saisir BC");
+            double bc = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Saisir AC");
+            double ac = Convert.ToDouble(Console.ReadLine());
+            if (ac == ab)
+            {
+                if (ac == bc)
+                {
+                    Console.WriteLine("Triangle équilatéral");
+                }
+                else
+                {
+                    Console.WriteLine("Triange isocéle en A");
+                }
+            }
+            else
+            {
+                if (ab == bc)
+                {
+                    Console.WriteLine("Triangle isocéle en B");
+                }
+                else if (ac == bc)
+                {
+                    Console.WriteLine("Triangel isocéle en C");
+                }
+                else
+                {
+                    Console.WriteLine("Ni isocéle ni équilatéral");
+                }
+            }
         }
     }
 }
