768947297ae218c4449fb6c073516a225ae165f1
ihab@utopios.net <ihab@utopios.net>
2020-08-31 11:57:50 +0200

    cours wpf partie 2

--- coursDotNet/coursWPF/MainWindow.xaml.cs
+++ coursDotNet/coursWPF/MainWindow.xaml.cs
@@ -25,24 +25,27 @@ namespace coursWPF
             InitializeComponent();
             //definition des Rows et des Columns
             grille.RowDefinitions.Add(new RowDefinition { Height = new GridLength(100) });
-            grille.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1,GridUnitType.Star) });
-            grille.RowDefinitions.Add(new RowDefinition { Height = new GridLength(2,GridUnitType.Star) });
-            grille.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1,GridUnitType.Auto) });
+            grille.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grille.RowDefinitions.Add(new RowDefinition { Height = new GridLength(2, GridUnitType.Star) });
+            grille.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
 
             grille.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(100) });
-            grille.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1,GridUnitType.Star) });
-            grille.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1,GridUnitType.Star) });
-            grille.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(2,GridUnitType.Auto) });
-            
+            grille.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            grille.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            grille.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(2, GridUnitType.Auto) });
+
 
             //Création du bouton
             Button b1 = new Button
             {
-                Content = "First Button"
+                Content = "First Button",
+                FontSize = 20,
+                //Background = Brushes.Red
+                Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#cd2127"))
             };
             //Ajout du bouton dans notre grille
             grille.Children.Add(b1);
-            
+
             //Placer le bouton dans la col N°3
             Grid.SetColumn(b1, 2);
             //Placer le bouton dans la ligne N°2
@@ -56,6 +59,13 @@ namespace coursWPF
             grille.Children.Add(b2);
             Grid.SetColumn(b2, 3);
             Grid.SetRow(b2, 3);
+            Label l = new Label
+            {
+                Content = "Contenu label"
+            };
+            grille.Children.Add(l);
+            Grid.SetColumn(l, 2);
+            Grid.SetRow(l, 0);
         }
     }
 }
