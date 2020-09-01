671e60f39b3e45f274cebf3c1b138162d4aa9176
ihab@utopios.net <ihab@utopios.net>
2020-09-01 10:55:05 +0200

    cours listBox

---
+++ coursDotNet/coursWPF/Personne.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coursWPF
+{
+    public class Personne
+    {
+        private string nom;
+
+        private string prenom;
+
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+
+        public override string ToString()
+        {
+            return Nom + " "+Prenom;
+        }
+    }
+}
---
+++ coursDotNet/coursWPF/WindowPersonne.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace coursWPF
+{
+    /// <summary>
+    /// Logique d'interaction pour WindowPersonne.xaml
+    /// </summary>
+    public partial class WindowPersonne : Window
+    {
+        private List<Personne> listePersonnes;
+        public WindowPersonne()
+        {
+            InitializeComponent();
+            listePersonnes = new List<Personne>();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //List<Personne> tmpPersonnes = new List<Personne>();
+            //tmpPersonnes.AddRange(listePersonnes);
+            ListeBoxPersonne.ItemsSource = null;
+            Personne p = new Personne
+            {
+                Nom = TextBoxNom.Text,
+                Prenom = TextBoxPrenom.Text
+            };
+            //tmpPersonnes.Add(p);
+            listePersonnes.Add(p);
+            ListeBoxPersonne.ItemsSource = listePersonnes;
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if(ListeBoxPersonne.SelectedItem is Personne p)
+            {
+                ResultPersonne.Text = p.ToString();
+            }
+        }
+    }
+}
