9b4f9e7adabf14dbd53eb4602b2218bd4c394b20
ihab@utopios.net <ihab@utopios.net>
2020-09-14 15:59:04 +0200

    cours asp.net core viewData

---
+++ coursDotNet/coursAspNET/Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coursAspNET.Models
+{
+    public class Contact
+    {
+        private string nom;
+
+        private string prenom;
+
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+    }
+}
