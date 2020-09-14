6978de2bc9fb2d8300bc10b8c93505aa00e4f0c9
ihab@utopios.net <ihab@utopios.net>
2020-09-14 15:54:42 +0200

    cours asp.net core viewData

--- coursDotNet/coursAspNET/Controllers/ContactController.cs
+++ coursDotNet/coursAspNET/Controllers/ContactController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using coursAspNET.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace coursAspNET.Controllers
@@ -10,6 +11,14 @@ namespace coursAspNET.Controllers
     {
         public IActionResult Index()
         {
+            Contact c = new Contact() { Nom = "toto", Prenom = "tata" };
+            ViewData["monContact"] = c;
+            List<Contact> contacts = new List<Contact>()
+            {
+                new Contact() { Nom = "titi", Prenom = "minet"},
+                new Contact() { Nom = "tt", Prenom = "ttt"},
+            };
+            ViewData["listeContacts"] = contacts;
             return View();
         }
 
