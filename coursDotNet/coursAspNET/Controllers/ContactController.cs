03dfcca2614e9dff431a1ed5c08874e02c1838b1
ihab@utopios.net <ihab@utopios.net>
2020-09-14 11:38:03 +0200

    cours asp.net core mvc 1

---
+++ coursDotNet/coursAspNET/Controllers/ContactController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coursAspNET.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Accueil()
+        {
+            return View();
+            //return new ContentResult() { Content = "<h1>Bonjour page contact</h1>" };
+        }
+    }
+}
