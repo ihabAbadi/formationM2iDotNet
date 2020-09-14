a7ae9709dfd0ea572abf43dd7855c3df2955ed74
ihab@utopios.net <ihab@utopios.net>
2020-09-14 15:07:17 +0200

    cours asp.net core correction ex1

---
+++ coursDotNet/EcoreAspNET/Controllers/EcoleController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcoreAspNET.Controllers
+{
+    public class EcoleController : Controller
+    {
+        public IActionResult Listes()
+        {
+            return View();
+        }
+
+        public IActionResult EtudiantForm()
+        {
+            return View();
+        }
+
+        public IActionResult ProfForm()
+        {
+            return View();
+        }
+    }
+}
