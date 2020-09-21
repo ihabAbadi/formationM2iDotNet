3da2ac4b838b2d93c71673ce339f8f147e5e03fa
ihab@utopios.net <ihab@utopios.net>
2020-09-21 10:20:05 +0200

    correction ajout product avec service upload

---
+++ coursDotNet/Ecommerce/Services/UploadService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Services
+{
+    public class UploadService
+    {
+        IWebHostEnvironment _env;
+
+        public UploadService(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+        public string Upload(IFormFile image)
+        {
+            string uniqueString = Guid.NewGuid().ToString();
+            string basePath = @"images/" + uniqueString + "-" + image.FileName;
+            string path = Path.Combine(_env.WebRootPath, basePath);
+            Stream s = System.IO.File.Create(path);
+            image.CopyTo(s);
+            s.Close();
+            return basePath;
+        }
+    }
+}
