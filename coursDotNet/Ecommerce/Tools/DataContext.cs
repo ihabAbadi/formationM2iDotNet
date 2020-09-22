4d3bf79ad6ba1ceee4ae59a51f46762b869a1e59
ihab@utopios.net <ihab@utopios.net>
2020-09-22 10:45:09 +0200

    dbContext entityframework

---
+++ coursDotNet/Ecommerce/Tools/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Ecommerce.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Tools
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Administrateur\Desktop\Formation\entity.mdf;Integrated Security=True;Connect Timeout=30");
+        }
+    }
+}
