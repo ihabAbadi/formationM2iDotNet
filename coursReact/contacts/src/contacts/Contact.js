7792bfebd0c09f97f40580f9e6625b0803348197
ihab@utopios.net <ihab@utopios.net>
2020-09-29 15:24:58 +0200

    correction edit contact

--- coursReact/contacts/src/contacts/Contact.js
+++ coursReact/contacts/src/contacts/Contact.js
@@ -23,16 +23,16 @@ import { Table, Input, InputNumber, Popconfirm, Form } from 'antd';
 // }
 
 export const Contact = (props) => {
-    console.log(props)
-    const { contact } = props
-    const [value, setValue] = useState(props.record != undefined ? props.record[props.dataIndex] : '')
-    const renderEmail = () => {
-        if (contact != undefined && contact.emails != undefined) {
-            return contact.emails.map((email) => (
-                <div className="row"><div className="col">{email.mail}</div><div onClick={() => props.deleteEmail(contact.id, email.id)}>X</div></div>
-            ))
-        }
-    }
+    // console.log(props)
+    // const { contact } = props
+    //const [value, setValue] = useState(props.record != undefined ? props.record[props.dataIndex] : '')
+    // const renderEmail = () => {
+    //     if (contact != undefined && contact.emails != undefined) {
+    //         return contact.emails.map((email) => (
+    //             <div className="row"><div className="col">{email.mail}</div><div onClick={() => props.deleteEmail(contact.id, email.id)}>X</div></div>
+    //         ))
+    //     }
+    // }
     return (
         // <div className="row">
         //     <div className="col-6">
@@ -52,7 +52,7 @@ export const Contact = (props) => {
             {(props.editing) ?
                 
                 (
-                    <Input defaultValue={value} onChange ={(e) => props.updateContactCell(props.dataIndex, e.target.value)} />
+                    <Input defaultValue={props.record != undefined ? props.record[props.dataIndex] : ''} onChange ={(e) => props.updateContactCell(props.dataIndex, e.target.value)} />
                 )
                 : (props.children)}
         </td>
