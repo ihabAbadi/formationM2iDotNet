012da1f428695898ddf6550e17f79543ac5efe43
ihab@utopios.net <ihab@utopios.net>
2020-10-02 16:47:16 +0200

    exemple wpf api

---
+++ coursDotNet/WpfApi/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApi
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            GetProducts();
+        }
+
+        private async void GetProducts()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer" ,"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9lbWFpbGFkZHJlc3MiOiJpaGFiQHV0b3Bpb3MubmV0IiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6ImFiYWRpIGloYWIiLCJleHAiOjE2MDE3MzM5ODQsImlzcyI6Im0yaSIsImF1ZCI6Im0yaSJ9.4bYE23YezvKggVn74a8Jc9FCA8mZ5bB5br17OPR_Qc0");
+            client.BaseAddress = new Uri(@"http://localhost:61692");
+            HttpResponseMessage response = await client.GetAsync("api/product");
+            string contenu = await response.Content.ReadAsStringAsync();
+        }
+    }
+}
